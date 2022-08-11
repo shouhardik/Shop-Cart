70c2338ee780fc35b1dea60dc7584ae27f01d215
Shouhardik Saha <60508616+shouhardik@users.noreply.github.com>
2022-08-11 15:39:47 +0530

    quant

--- main.js
+++ main.js
@@ -44,3 +44,44 @@ closeCart.onclick=()=>{
     console.log('close');
     cart.remove("active")
 }
+
+if(document.readyState=="loading"){
+    document.addEventListener("DOMContentLoaded",ready)
+}else{
+    ready()
+}
+
+function ready(){
+    var removeCartButtons=document.getElementsByClassName("cart-remove");
+    console.log(removeCartButtons);
+    for(var i=0;i<removeCartButtons.length;i++){
+        var button=removeCartButtons[i]
+        button.addEventListener('click',removeCartItem)
+    }
+    //Quantity Change
+}
+//Remove items
+function removeCartItem(event){
+    var  buttonClicked = event.target
+    buttonClicked.parentElement.remove()
+    updatetotal();
+}
+
+//Update Items
+
+function updatetotal(){
+    var cartContent=document.getElementsByClassName("cart-content")[0];
+    var cartBoxes = cartContent.getElementsByClassName("cart-box")
+    var total=0;
+    for(var i=0;i<cartBoxes.length;i++){
+        var cartBox=cartBoxes[i];
+        var priceElement=cartBox.getElementsByClassName("cart-price")[0]
+        var quantityElement=cartBox.getElementsByClassName("cart-quantity")[0]
+        var price=parseFloat(priceElement.innerText.replace("$",""))
+        var quantity=quantityElement.value
+        total=total+(price*quantity);
+
+        document.getElementsByClassName("total-price")[0].innerText("$"+total)
+    }
+
+}
